921b1db314a5db6a5fa9010812a000153e2c45fc
thibert-derek <thibertderek@gmail.com>
2023-03-15 17:27:09 -0400

    Made some more changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,9 @@ const Contact = () => {
         action="https://getform.io/f/316eb73c-f136-4dbb-92a1-5472734c2d7f" 
         enctype="multipart/form-data"
         className="flex flex-col max-w-[1000px] px-20">
-            <input className="p-2 my-2 bg-[#ccd6f6] text-black" type="text" placeholder="Name" name="name"/>
-            <input className="p-2 my-2 bg-[#ccd6f6] text-black" type="email" placeholder="Email" name="email"/>
-            <textarea className="p-2 my-2 bg-[#ccd6f6] text-black" cols="30" rows="10" type="message" placeholder="Message" name="name"/>
+            <input className="p-2 my-2 bg-[#ffffff] text-black" type="text" placeholder="Name" name="name"/>
+            <input className="p-2 my-2 bg-[#ffffff] text-black" type="email" placeholder="Email" name="email"/>
+            <textarea className="p-2 my-2 bg-[#ffffff] text-black" cols="30" rows="10" type="message" placeholder="Message" name="name"/>
             <button className="border-2 items-center hover:bg-white hover:text-black duration-300 px-4 py-3 my-8 mx-auto flex">Send</button>
             </form>
     </div>
