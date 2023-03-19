99bc2410ab2735360537fea26c4a36550c078f81
thibert-derek <thibertderek@gmail.com>
2023-03-19 12:52:45 -0400

    Added New Project Box

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import weather from '../assets/weather5.jpeg'
 import JNE from '../assets/JNE2.jpeg'
+import tictactoe from '../assets/ttt.jpeg'
 
 const Projects = () => {
     return (
@@ -21,11 +22,11 @@ const Projects = () => {
                     <div>
                         <div className="grid grid-cols-1 lg:grid-cols-2 px-10 sm:px-20 py-10 justify-center gap-4 items-center">
                             
-                            <span className='justify-center my-10 w-[100%] items-center border-2'>
+                            <span className='justify-center my-5 w-[100%] items-center border-2'>
                                 <div>
                                     <img className="mx-auto lg:h-[350px] lg:w-[500px]" src={weather} />
                                 </div>
-                                <div className="px-4 py-4">
+                                <div className="px-6 py-4">
                                     <h3 className="font-bold text-2xl text-[#00FFBF]">Eye To The Sky</h3>
                                     <p className="py-4">Personal project building a weather application. Used OpenWeatherMap API, React and Tailwind.</p>
                                     <div className="justify-between items-right">
@@ -38,16 +39,33 @@ const Projects = () => {
                                     </div>
                                 </div>
                             </span>
-                            <span className='justify-center my-10 w-[100%] items-center border-2'>
+                            <span className='justify-center my-5 w-[100%] items-center border-2'>
                                 <div>
                                     <img className="mx-auto lg:h-[350px] lg:w-[500px]" src={JNE} />
                                 </div>
-                                <div className="px-4 py-4">
+                                <div className="px-6 py-4">
                                     <h3 className="font-bold text-2xl text-[#00FFBF]">JNE Contracting</h3>
                                     <p className="py-4">Developed the website for a local contracting business. Used WordPress, CSS and HTML.</p>
                                     <div className="justify-between items-right">
                                         <a className="px-4 my-2 border-2 hover:bg-white hover:text-black duration-300" href="https://jnecontracting.ca/">
                                         Site
+                                        </a>
+                                        {/* <a className="px-4 mx-2 my-2 border-2 md:hover:bg-white md:hover:text-black md:duration-300" href="https://github.com/thibert-derek/eyetothesky">
+                                            Code
+                                        </a> */}
+                                    </div>
+                                </div>
+                            </span>
+                            <span className='justify-center my-5 w-[100%] items-center border-2'>
+                                <div>
+                                    <img className="mx-auto lg:h-[350px] lg:w-[500px]" src={tictactoe} />
+                                </div>
+                                <div className="px-6 py-4">
+                                    <h3 className="font-bold text-2xl text-[#00FFBF]">Tic Tac Toe</h3>
+                                    <p className="py-4">A school project take on a classic 2 player game. Used JavaScript, CSS and HTML.</p>
+                                    <div className="justify-between items-right">
+                                        <a className="px-4 my-2 border-2 hover:bg-white hover:text-black duration-300" href="https://github.com/thibert-derek/TicTacToe">
+                                        Code
                                         </a>
                                         {/* <a className="px-4 mx-2 my-2 border-2 md:hover:bg-white md:hover:text-black md:duration-300" href="https://github.com/thibert-derek/eyetothesky">
                                             Code
