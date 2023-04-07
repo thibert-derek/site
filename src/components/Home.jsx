3b06cf0c63666883f993090634458c697ce49f45
thibert-derek <thibertderek@gmail.com>
2023-04-07 13:04:57 -0400

    Added Resume Button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Face from '../assets/face.jpg'
 import {FaLinkedin, FaGithub} from 'react-icons/fa'
 import {SiFiverr, SiUpwork} from 'react-icons/si'
+import Resume from '../assets/DerekThibert_CV.pdf'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
             </div>
             <h1 className='text-[#00FFBF] font-bold text-4xl md:text-7xl'>Derek Thibert</h1>
             <h2 className ='text-3xl md:text-5xl py-2'>Software Developer</h2>
+            <button className="text-2xl border-2 items-center hover:bg-white hover:text-black duration-300 px-4 py-3 my-8 mx-auto flex"><a href={Resume} download="Resume">Resume</a></button>
             {/* <p className ='text-2xl py-2'>Let's build together.</p> */}
             {/* <ul className="flex">
               <li className = "py-2 text-4xl hover:scale-150 duration-300"><a href="https://www.linkedin.com/in/derek-thibert/"><FaLinkedin /></a></li>
